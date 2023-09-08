f767ec05000b917ed0991766cb679ed811697eb6
Hiroki Akahoshi <akaho1421@gmail.com>
2023-09-09 01:27:37 +0900

    fix: 座標ずれを直した

--- stlConverter.ts
+++ stlConverter.ts
@@ -5,72 +5,69 @@ import { Carpet } from "./lib/carpet";
 import { BLOCK_DATA } from "./lib/blockData/blockData";
 
 /** 配列形式のブロックデータをascii-stlに変換する */
-function stlConvert(structuredata: [[[number]]]) : string {
-    let resultStringStl = "solid result";
+function stlConvert(structuredata: [[[number]]]): string {
+  let resultStringStl = "solid result";
 
-    structuredata.forEach((y, i) => {
-        y.forEach((x, j) => {
-            x.forEach((z, k) => {
+  structuredata.forEach((y, i) => {
+    y.forEach((x, j) => {
+      x.forEach((z, k) => {
+        //空気ブロックではないなら
+        if (z !== 0) {
+          switch (z) {
+            case BLOCK_DATA.NORMAL:
+              resultStringStl += new Block(j, i, k).block();
+              break;
 
-                //空気ブロックではないなら
-                if(z !== 0){
-                    switch(z){
-                        case BLOCK_DATA.NORMAL:
-                            resultStringStl += new Block(i, j, k).block();
-                            break;
+            case BLOCK_DATA.CARPET: // カーペット
+              resultStringStl += new Carpet(j, i, k).carpet();
+              break;
 
-                        case BLOCK_DATA.CARPET: // カーペット
-                            resultStringStl += new Carpet(j, i, k).carpet();
-                            break;
+            case BLOCK_DATA.HALF_BLOCK_UP: // ハーフブロック(上)
+              resultStringStl += new HalfBlock(j, i, k).up();
+              break;
 
-                        case BLOCK_DATA.HALF_BLOCK_UP: // ハーフブロック(上)
-                            resultStringStl += new HalfBlock(i, j, k).up();
-                            break;
+            case BLOCK_DATA.HALF_BLOCK_DOWN: // ハーフブロック(下)
+              resultStringStl += new HalfBlock(j, i, k).down();
+              break;
 
-                        case BLOCK_DATA.HALF_BLOCK_DOWN: // ハーフブロック(下)
-                            resultStringStl += new HalfBlock(i, j, k).down();
-                            break;
-                        
-                        case BLOCK_DATA.STAIR_BLOCK_DOWN_X_PLUS: // 階段ブロック(下、X+)
-                            resultStringStl += new StairBlock(i, j, k, "x-plus").down();
-                            break;
-                        
-                        case BLOCK_DATA.STAIR_BLOCK_DOWN_X_MINUS: // 階段ブロック(下、X-)
-                            resultStringStl += new StairBlock(i, j, k, "x-minus").down();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_DOWN_X_PLUS: // 階段ブロック(下、X+)
+              resultStringStl += new StairBlock(j, i, k, "x-plus").down();
+              break;
 
-                        case BLOCK_DATA.STAIR_BLOCK_DOWN_Z_PLUS: // 階段ブロック(下、Z+)
-                            resultStringStl += new StairBlock(i, j, k, "z-plus").down();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_DOWN_X_MINUS: // 階段ブロック(下、X-)
+              resultStringStl += new StairBlock(j, i, k, "x-minus").down();
+              break;
 
-                        case BLOCK_DATA.STAIR_BLOCK_DOWN_Z_MINUS: // 階段ブロック(下、Z-)
-                            resultStringStl += new StairBlock(i, j, k, "z-minus").down();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_DOWN_Z_PLUS: // 階段ブロック(下、Z+)
+              resultStringStl += new StairBlock(j, i, k, "z-plus").down();
+              break;
 
-                        case BLOCK_DATA.STAIR_BLOCK_UP_X_PLUS: // 階段ブロック(上、X+)
-                            resultStringStl += new StairBlock(i, j, k, "x-plus").up();
-                            break;
-                        
-                        case BLOCK_DATA.STAIR_BLOCK_UP_X_MINUS: // 階段ブロック(上、X-)
-                            resultStringStl += new StairBlock(i, j, k, "x-minus").up();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_DOWN_Z_MINUS: // 階段ブロック(下、Z-)
+              resultStringStl += new StairBlock(j, i, k, "z-minus").down();
+              break;
 
-                        case BLOCK_DATA.STAIR_BLOCK_UP_Z_PLUS: // 階段ブロック(上、Z+)
-                            resultStringStl += new StairBlock(i, j, k, "z-plus").up();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_UP_X_PLUS: // 階段ブロック(上、X+)
+              resultStringStl += new StairBlock(j, i, k, "x-plus").up();
+              break;
 
-                        case BLOCK_DATA.STAIR_BLOCK_UP_Z_MINUS: // 階段ブロック(上、Z-)
-                            resultStringStl += new StairBlock(i, j, k, "z-minus").up();
-                            break;
+            case BLOCK_DATA.STAIR_BLOCK_UP_X_MINUS: // 階段ブロック(上、X-)
+              resultStringStl += new StairBlock(j, i, k, "x-minus").up();
+              break;
 
-                    }
+            case BLOCK_DATA.STAIR_BLOCK_UP_Z_PLUS: // 階段ブロック(上、Z+)
+              resultStringStl += new StairBlock(j, i, k, "z-plus").up();
+              break;
 
-                }
-            });
-        });
+            case BLOCK_DATA.STAIR_BLOCK_UP_Z_MINUS: // 階段ブロック(上、Z-)
+              resultStringStl += new StairBlock(j, i, k, "z-minus").up();
+              break;
+          }
+        }
+      });
     });
-    
-    return resultStringStl + "endsolid\n";
-} 
+  });
 
-export {stlConvert};
+  return resultStringStl + "endsolid\n";
+}
+
+export { stlConvert };
