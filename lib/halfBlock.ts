a46ac31de5781fa71d7fbb597ef77bffbb0e455b
akahoshi1421 <akaho1421@gmail.com>
2023-09-08 14:00:03 +0900

    ハーフブロックのSTL完成

--- lib/halfBlock.ts
+++ lib/halfBlock.ts
@@ -6,11 +6,189 @@ class HalfBlock extends Block{
     }
 
     up() {
-        
+        return `
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y + 1}.000000e+01  ${super.z}.000000e+01       
+            endloop
+        endfacet`   
     }
 
     down() {
-        
+        return `
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z + 1}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z + 1}.000000e+01
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01
+                vertex  ${super.x + 1}.000000e+01  ${super.y}.000000e+01  ${super.z}.000000e+01
+                vertex  ${super.x}.000000e+01  ${super.y}.500000e+01  ${super.z}.000000e+01       
+            endloop
+        endfacet`
     }   
 }
 
