c8f6d5df42babbbedc8569f6d55c99b2410cbd79
akahoshi1421 <akaho1421@gmail.com>
2023-09-21 21:34:15 +0900

    add: 石フェンス(2個接続) x+x-とz+z-以外完成

---
+++ lib/fence/stoneFence/two/xPlusZPlus.ts
@@ -0,0 +1,397 @@
+import { Vector3 } from "../../../vector3";
+
+class TwoXPlusZPlus extends Vector3{
+    private xPlusHeigher: boolean = false;
+    private zPlusHeigher: boolean = false;
+
+    constructor(x: number, y: number, z: number, xPlusHeigher: boolean, zPlusHeigher: boolean){
+        super(x, y, z);
+        this.xPlusHeigher = xPlusHeigher;
+        this.zPlusHeigher = zPlusHeigher;
+    }
+
+    xPlusZPlus(){
+        return `
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.250000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.250000e+01
+
+                vertex  ${this.x}.250000e+01  ${this.y+1}.000000e+01  ${this.z}.250000e+01
+            endloop
+        endfacet
+
+
+
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.687500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.687500e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.750000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.312500e+01
+
+                vertex  ${this.x}.750000e+01  ${this.xPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.312500e+01
+            endloop
+        endfacet
+
+
+
+
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.312500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.687500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+
+                vertex  ${this.x}.687500e+01  ${this.y}.000000e+01  ${this.z}.750000e+01
+
+                vertex  ${this.x}.312500e+01  ${this.zPlusHeigher ? `${this.y+1}.000000e+01` : `${this.y}.800000e+01` }  ${this.z}.750000e+01
+            endloop
+        endfacet
+
+        `
+    }
+}
