a9731cf1d5a9e48e22dc0845fc623cdea0c5ba40
akahoshi1421 <akaho1421@gmail.com>
2023-10-04 01:25:12 +0900

    add: 階段端(上、凸)を追加

---
+++ lib/stair/corner/up-corner/totu/x-minus-z-plus.ts
@@ -0,0 +1,268 @@
+import { Vector3 } from "../../../../vector3";
+
+class XMinusZPlus extends Vector3{
+    constructor(x: number, y: number, z: number){
+        super(x, y, z);
+    }
+
+    xMinusZplus(){
+        return `
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z+1}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z+1}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.000000e+01  ${this.y+1}.000000e+01  ${this.z}.000000e+01
+            endloop
+        endfacet
+
+
+
+
+
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  1.000000e+01  0.000000e+00  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+01  -1.000000e+01  0.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.500000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.500000e+01
+
+            endloop
+        endfacet
+
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x}.500000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+            endloop
+        endfacet
+        facet normal  0.000000e+00  0.000000e+00  -1.000000e+01
+            outer loop
+                vertex  ${this.x+1}.000000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x+1}.000000e+01  ${this.y}.000000e+01  ${this.z}.000000e+01
+
+                vertex  ${this.x}.500000e+01  ${this.y}.500000e+01  ${this.z}.000000e+01
+            endloop
+        endfacet
+
+        `
+    }
+}
+
+export { XMinusZPlus }
